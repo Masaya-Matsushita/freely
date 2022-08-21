d1d9139bc29912e4d562985179945f2b7c86dc62
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-22 05:31:12 +0900

    ➕ Mantineを導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@mantine/core'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
       <div className='text-lg text-red-500'>Hello World!</div>
+      <Button>btn</Button>
     </>
   )
 }
