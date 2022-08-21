c8bb92da19e63a70b78373da83eb6f6df08cce73
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-22 05:49:54 +0900

    ✨ MantineのuseMediaQueryフックを拡張

---
+++ src/libs/mantine/useMediaQuery.ts
@@ -0,0 +1,18 @@
+import { useMediaQuery as useMediaQueryOriginal } from '@mantine/hooks'
+
+const map = {
+  xxs: '320px',
+  xs: '576px',
+  sm: '768px',
+  md: '992px',
+  lg: '1200px',
+  xl: '1400px',
+} as const
+
+// useMediaQueryの第1引数をブレークポイントの値のみに制限するフック
+export const useMediaQuery = (
+  query: keyof typeof map,
+  initialValue: Parameters<typeof useMediaQueryOriginal>[1] = true,
+) => {
+  return useMediaQueryOriginal(`(min-width: ${map[query]})`, initialValue)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import { Button } from '@mantine/core'
 import type { NextPage } from 'next'
+import { useMediaQuery } from '@/libs/mantine/useMediaQuery'
 
 const Home: NextPage = () => {
+  const largerThanXxs = useMediaQuery('xxs')
+  const largerThanXs = useMediaQuery('xs')
+  const largerThanSm = useMediaQuery('sm')
+  const largerThanMd = useMediaQuery('md')
+  const largerThanLg = useMediaQuery('lg')
+  const largerThanXl = useMediaQuery('xl')
+
   return (
     <>
       <div className='text-lg text-red-500'>Hello World!</div>
-      <Button>btn</Button>
+      <Button mt={largerThanXxs ? 'lg' : 'sm'}>btn</Button>
     </>
   )
 }
