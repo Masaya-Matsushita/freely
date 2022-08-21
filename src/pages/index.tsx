8796261eb1444d218f3df13c2da8ceddb2f4bb72
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-22 04:57:39 +0900

    🎨 Tailwind CSSの設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <>
-      <div className='text-red-500'>Hello World!</div>
+      <div className='text-lg text-red-500'>Hello World!</div>
     </>
   )
 }
