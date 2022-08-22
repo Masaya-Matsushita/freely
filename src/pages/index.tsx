9cafe0fb16d55dd1a822bd65573a1fa6401294fc
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-22 20:40:29 +0900

    ➕ react-error-boundaryを導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
 import { Button } from '@mantine/core'
 import type { NextPage } from 'next'
+import { useErrorHandler } from 'react-error-boundary'
 import { useMediaQuery } from '@/lib/mantine/useMediaQuery'
 
 const Home: NextPage = () => {
   const largerThanXxs = useMediaQuery('xxs')
-  const largerThanXs = useMediaQuery('xs')
-  const largerThanSm = useMediaQuery('sm')
-  const largerThanMd = useMediaQuery('md')
-  const largerThanLg = useMediaQuery('lg')
-  const largerThanXl = useMediaQuery('xl')
+
+  const handleError = useErrorHandler()
+
+  const handleClick = () => {
+    try {
+      throw new Error('Error is occurred!')
+    } catch (error: any) {
+      handleError(error)
+    }
+  }
 
   return (
     <>
       <div className='text-lg text-red-500'>Hello World!</div>
-      <Button mt={largerThanXxs ? 'lg' : 'sm'}>btn</Button>
+      <Button mt={largerThanXxs ? 'lg' : 'sm'} onClick={handleClick}>
+        btn
+      </Button>
     </>
   )
 }
