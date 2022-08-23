8030e4527ab84668fc901055f0c822521ee78c78
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-23 18:08:18 +0900

    ♻️ AppMantineProviderコンポーネントを作成

---
+++ src/lib/mantine/AppMantineProvider.tsx
@@ -0,0 +1,16 @@
+import { MantineProvider } from '@mantine/core'
+import { FC, ReactNode } from 'react'
+
+export const AppMantineProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: 'light',
+      }}
+    >
+      {children}
+    </MantineProvider>
+  )
+}
