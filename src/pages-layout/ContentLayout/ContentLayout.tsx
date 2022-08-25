898e46c9bbe565c8aff315f1f6c6dc3f697a6696
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-25 18:26:49 +0900

    💄 レイアウトを調整

--- src/pages-layout/ContentLayout/ContentLayout.tsx
+++ src/pages-layout/ContentLayout/ContentLayout.tsx
@@ -1,8 +1,8 @@
 import { Burger, CloseButton, Drawer, UnstyledButton } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { IconArrowLeft, IconArrowRight, IconShare } from '@tabler/icons'
-import { useRouter } from 'next/router'
-import { ReactElement, useEffect } from 'react'
+import { NextRouter, useRouter } from 'next/router'
+import { FC, ReactElement, useEffect } from 'react'
 import { NavLinks } from './NavLinks'
 import { ShareModal } from './ShareModal'
 import { ErrorBoundary } from 'src/pages-layout/ErrorBoundary'
@@ -12,43 +12,51 @@ import { HeaderWrapper } from 'src/pages-layout/HeaderWrapper'
  * @package
  */
 export const ContentLayout = (page: ReactElement) => {
+  const router = useRouter()
+  const planId = router.query.planId
+
   return (
     <ErrorBoundary>
-      <div className='min-h-screen'>
-        <Header />
-        <div className='flex'>
-          <SideNav />
-          <main className='flex-1 bg-main-100'>{page}</main>
+      {typeof planId === 'string' ? (
+        <div>
+          <Header router={router} planId={planId} />
+          <div className='flex'>
+            <SideNav planId={planId} />
+            <main className='min-h-[calc(100vh-96px)] flex-1 bg-main-100'>
+              {page}
+            </main>
+          </div>
+          <footer>Footer</footer>
         </div>
-        <footer>Footer</footer>
-      </div>
+      ) : null}
     </ErrorBoundary>
   )
 }
 
-const Header = () => {
-  const router = useRouter()
-  const planId = router.query.planId
+type HeaderProps = {
+  router: NextRouter
+  planId: string
+}
+
+const Header: FC<HeaderProps> = (props) => {
   const [drawerOpened, drawerHandlers] = useDisclosure(false)
   const [modalOpened, modalHandlers] = useDisclosure(false)
 
   // パスの変更を検知して Drawer を閉じる処理
   useEffect(() => {
-    router.events.on('routeChangeStart', drawerHandlers.close)
+    props.router.events.on('routeChangeStart', drawerHandlers.close)
     return () => {
-      router.events.off('routeChangeStart', drawerHandlers.close)
+      props.router.events.off('routeChangeStart', drawerHandlers.close)
     }
-  }, [drawerHandlers, router.events])
+  }, [drawerHandlers, props.router.events])
 
   return (
     <HeaderWrapper>
-      {typeof planId === 'string' ? (
-        <ShareModal
-          planId={planId}
-          opened={modalOpened}
-          close={modalHandlers.close}
-        />
-      ) : null}
+      <ShareModal
+        planId={props.planId}
+        opened={modalOpened}
+        close={modalHandlers.close}
+      />
       <Drawer
         opened={drawerOpened}
         size={'65%'}
@@ -66,9 +74,9 @@ const Header = () => {
           Freely
         </div>
         <hr className='mx-2 h-[1px] border-0 bg-dark-100' />
-        {typeof planId === 'string' ? <NavLinks planId={planId} /> : null}
+        <NavLinks planId={props.planId} />
       </Drawer>
-      <div className='flex items-center gap-8'>
+      <div className='flex items-center gap-8 xs:ml-4'>
         <Burger
           opened={drawerOpened}
           onClick={() => drawerHandlers.open()}
@@ -76,7 +84,7 @@ const Header = () => {
           color='#495057'
           className='ml-6 sm:hidden'
         />
-        <div className='text-2xl font-medium tracking-wider text-dark-500'>
+        <div className='text-2xl font-medium tracking-wider text-dark-500 xs:text-3xl xs:font-normal sm:ml-4'>
           Freely
         </div>
       </div>
@@ -84,7 +92,7 @@ const Header = () => {
         size={32}
         color='#6466F1'
         stroke={1.8}
-        className='mr-6'
+        className='mr-6 xs:mr-10 md:mr-16'
         onClick={() => {
           modalHandlers.open()
         }}
@@ -93,28 +101,29 @@ const Header = () => {
   )
 }
 
-const SideNav = () => {
-  const router = useRouter()
-  const planId = router.query.planId
+const SideNav: FC<{ planId: string }> = (props) => {
   const [opened, handlers] = useDisclosure(true)
 
   return (
     <nav className='hidden sm:block'>
       <div
         style={{ transition: 'all 0.3s' }}
-        className={`flex h-screen ${
+        className={`flex ${
           opened ? 'w-[276px]' : 'w-[90px]'
-        } flex-col justify-between border-solid border-white border-r-slate-200`}
+        } min-h-[calc(100vh-96px)] flex-col justify-between border-solid border-white border-r-slate-200`}
       >
-        {typeof planId === 'string' ? <NavLinks planId={planId} /> : null}
-        <div className='flex flex-col'>
+        <NavLinks planId={props.planId} />
+        <div className='mb-8 flex flex-col'>
           <hr
             style={{ transition: 'all 0.3s' }}
             className={`mx-2 h-[1px] ${
               opened ? 'w-64' : 'w-[72px]'
             } border-0 bg-slate-300`}
           />
-          <UnstyledButton onClick={() => handlers.toggle()} className='mx-8 '>
+          <UnstyledButton
+            onClick={() => handlers.toggle()}
+            className='mx-8 mt-2 hover:bg-slate-100'
+          >
             {opened ? (
               <div className='flex h-7 items-center gap-2 text-slate-400'>
                 <IconArrowLeft color='#AFAFAF' stroke={2} />
