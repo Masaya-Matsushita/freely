7455d6bf8b8d5ac6d922ae2564b9357dcaeab59b
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-08-28 10:02:10 +0900

    ➕ swrを導入

--- src/pages-component/pref-news/page.tsx
+++ src/pages-component/pref-news/page.tsx
@@ -1,13 +1,19 @@
-import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+const fetcher = async (input: RequestInfo, init?: RequestInit) => {
+  const res = await fetch(input, init)
+  return res.json()
+}
 
 /**
  * @package
  */
 export const PrefNews = () => {
-  const { asPath } = useRouter()
-  return (
-    <div>
-      <div className='text-lg text-blue-500'>{asPath}</div>
-    </div>
+  const { data, error } = useSWR(
+    'https://jsonplaceholder.typicode.com/todos/1',
+    fetcher,
   )
+  console.log(data)
+
+  return <div></div>
 }
