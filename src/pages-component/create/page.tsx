077253f14823ec9f4e7fe16c3941259856dc6766
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-09-02 22:06:17 +0900

    ✨ PasswordModalコンポーネントを作成

--- src/pages-component/create/page.tsx
+++ src/pages-component/create/page.tsx
@@ -1,7 +1,7 @@
-import { Button, Modal, TextInput } from '@mantine/core'
+import { Button } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { PasswordModal } from 'src/component/PasswordModal'
 import { SimpleButton } from 'src/component/SimpleButton'
 import { getPath } from 'src/lib/const'
 
@@ -10,34 +10,11 @@ import { getPath } from 'src/lib/const'
  */
 export const Create = () => {
   const router = useRouter()
-  const planId = router.query.plan
-  const [password, setPassword] = useState('')
   const [opened, handlers] = useDisclosure(false)
-
-  const handleSubmit = () => {
-    if (!localStorage.hasOwnProperty('password')) {
-      handlers.open()
-    }
-    console.log('編集しました。')
-  }
-
-  const handleAuth = async () => {
-    const authRes = await fetch('/api/auth', {
-      method: 'POST',
-      headers: { 'Content-type': 'application/json' },
-      body: JSON.stringify({ planId: planId, password: password }),
-    })
-    const authJson = await authRes.json()
-    if (authJson) {
-      localStorage.setItem('password', password)
-      handleSubmit()
-    } else {
-      console.log('パスワードが違います。')
-    }
-  }
 
   return (
     <div>
+      <PasswordModal opened={opened} closeModal={handlers.close} />
       <div className='mt-20'>
         <SimpleButton
           text='作成する'
@@ -49,21 +26,7 @@ export const Create = () => {
           }}
         />
       </div>
-      <Button onClick={handleSubmit}>submit</Button>
-      <Modal
-        opened={opened}
-        onClose={() => handlers.close()}
-        title='Introduce yourself!'
-      >
-        <TextInput
-          placeholder='Password'
-          label='Password'
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-          withAsterisk
-        />
-        <Button onClick={handleAuth}>auth</Button>
-      </Modal>
+      <Button onClick={() => handlers.open()}>open</Button>
     </div>
   )
 }
