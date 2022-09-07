3267aaebf54dc5ac4780821443d0f1b86d36a759
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-09-08 01:58:49 +0900

    🐛 API側のエンドポイントの変更

--- src/pages/api/memoList.ts
+++ src/pages/api/memoList.ts
@@ -1,10 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 const url = require('url')
 
-const spot = async (req: NextApiRequest, res: NextApiResponse) => {
+const memoList = async (req: NextApiRequest, res: NextApiResponse) => {
   const params = await url.parse(req.url, true).query
-  const data = await fetch(`http://0.0.0.0/memo?spot_id=${params.spotId}`)
+  const data = await fetch(`http://0.0.0.0/memo-list?spot_id=${params.spotId}`)
   res.status(200).json(await data.json())
 }
 
-export default spot
+export default memoList
--- src/pages/api/spotList.ts
+++ src/pages/api/spotList.ts
@@ -3,7 +3,7 @@ const url = require('url')
 
 const spotList = async (req: NextApiRequest, res: NextApiResponse) => {
   const params = await url.parse(req.url, true).query
-  const data = await fetch(`http://0.0.0.0/spot_list?plan_id=${params.planId}`)
+  const data = await fetch(`http://0.0.0.0/spot-list?plan_id=${params.planId}`)
   res.status(200).json(await data.json())
 }
 
