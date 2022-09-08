f561e4e180cf527c7698ac0c69ce5a4a0f4007e6
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-09-08 21:13:22 +0900

    ✨ memoの値を状態管理

--- src/component/MemoModal/MemoModal.tsx
+++ src/component/MemoModal/MemoModal.tsx
@@ -7,7 +7,7 @@ import {
 } from '@mantine/core'
 import { NextLink } from '@mantine/next'
 import { IconDots, IconPencil, IconTrash } from '@tabler/icons'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { AiOutlineSend } from 'react-icons/ai'
 import { FaPen } from 'react-icons/fa'
 
@@ -23,6 +23,13 @@ type Props = {
  * @package
  */
 export const MemoModal: FC<Props> = (props) => {
+  const [marked, setMarked] = useState(false)
+  const [value, setValue] = useState('')
+
+  const handleSubmit = () => {
+    console.log(value)
+  }
+
   return (
     <Modal
       opened={props.opened}
@@ -31,7 +38,7 @@ export const MemoModal: FC<Props> = (props) => {
       centered
       size='lg'
       classNames={{
-        modal: 'p-0 pt-4 relative rounded-lg xs:mx-8 mx-4 max-w-[480px]',
+        modal: 'p-0 pt-4 relative rounded-lg xs:mx-8 xxs:mx-4 max-w-[450px]',
       }}
     >
       <CloseButton
@@ -41,9 +48,9 @@ export const MemoModal: FC<Props> = (props) => {
         onClick={props.close}
         className='absolute -top-8 right-3'
       />
-      <div className='mx-4 mb-3 flex justify-between'>
+      <div className='mx-2 mb-3 flex justify-between xxs:mx-4'>
         <div className='ml-2 text-xl text-dark-500'>東京スカイツリー</div>
-        <Menu shadow='md' width={200} position='bottom-end' offset={3}>
+        <Menu shadow='md' width={160} position='bottom-end' offset={3}>
           <Menu.Target>
             <UnstyledButton className='rounded-md'>
               <IconDots color='#999999' size={28} />
@@ -67,13 +74,15 @@ export const MemoModal: FC<Props> = (props) => {
           </Menu.Dropdown>
         </Menu>
       </div>
-      <div className='h-[360px] space-y-4 overflow-auto border-[1px] border-solid border-main-200 border-y-dark-100 bg-main-200 py-6 px-4'>
-        {props.memoList.map((memo) => {
-          return (
-            <div key={memo.id} className='flex items-center gap-2'>
-              <div className='flex-1 rounded-md bg-white'>
-                <div
-                  className={`mx-1 rounded-sm border-[0.1px] border-l-4 border-solid border-white p-2 text-sm text-dark-600
+      <div className='h-[360px] overflow-auto border-[1px] border-solid border-main-200 border-y-dark-100 bg-main-200 py-6 px-4 xs:h-[400px] xs:px-6'>
+        {props.memoList.length ? (
+          <div className='space-y-4'>
+            {props.memoList.map((memo) => {
+              return (
+                <div key={memo.id} className='flex items-center gap-2 xs:gap-3'>
+                  <div className='flex-1 rounded-md bg-white'>
+                    <div
+                      className={`mx-1 rounded-sm border-[0.1px] border-l-4 border-solid border-white p-2 text-sm text-dark-600 xs:text-base
                   ${
                     memo.marked === 'Red'
                       ? 'border-l-red-500'
@@ -81,29 +90,40 @@ export const MemoModal: FC<Props> = (props) => {
                       ? 'border-l-green-400'
                       : null
                   }`}
-                >
-                  {memo.text}
+                    >
+                      {memo.text}
+                    </div>
+                  </div>
+                  <UnstyledButton className='rounded-md'>
+                    <IconTrash color='#fff' size={30} />
+                  </UnstyledButton>
                 </div>
-              </div>
-              <UnstyledButton className='rounded-md'>
-                <IconTrash color='#fff' size={30} />
-              </UnstyledButton>
-            </div>
-          )
-        })}
+              )
+            })}
+          </div>
+        ) : (
+          <div className='mt-36 text-center text-dark-300'>
+            メモはありません
+          </div>
+        )}
       </div>
-      <div className='mx-4 flex items-center gap-3 py-3'>
-        <UnstyledButton className='mt-1 rounded-md'>
-          <FaPen color={'#ff2626'} size={22} />
+      <div className='mx-2 flex items-center gap-3 pt-3 pb-4 xxs:mx-4 xs:mx-6 xs:gap-4'>
+        <UnstyledButton
+          onClick={() => setMarked((prev) => !prev)}
+          className='mt-1 rounded-md'
+        >
+          <FaPen color={marked ? '#ff2626' : '#798087'} size={22} />
         </UnstyledButton>
         <TextInput
           placeholder='時間、料金、持ち物など'
+          value={value}
+          onChange={(e) => setValue(e.currentTarget.value)}
           size={'sm'}
           classNames={{ input: 'rounded-2xl bg-slate-100' }}
           className='flex-1'
         />
-        <UnstyledButton className='mt-1 rounded-md'>
-          <AiOutlineSend color='#495057' size={26} />
+        <UnstyledButton onClick={handleSubmit} className='mt-1 rounded-md'>
+          <AiOutlineSend color='#495057' size={28} />
         </UnstyledButton>
       </div>
     </Modal>
