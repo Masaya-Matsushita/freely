9e2ef5eecc445df138fb1f34088791dcb8bc1956
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-09-13 22:32:57 +0900

    ✨ マウント時のGlobalState, LocalStorageへのデータ設定処理を実装

--- src/pages-layout/ContentLayout/InitState.ts
+++ src/pages-layout/ContentLayout/InitState.ts
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useSetRecoilState } from 'recoil'
+import { sortAndSavePlanList } from 'src/lib/func'
+import { passwordState } from 'src/state/password'
 import { planIdState } from 'src/state/planId'
 import { prefIdState } from 'src/state/prefId'
 
@@ -12,35 +14,44 @@ export const InitState = () => {
   const planId = router.query.planId
   const setPlanId = useSetRecoilState(planIdState)
   const setPrefId = useSetRecoilState(prefIdState)
+  const setPassword = useSetRecoilState(passwordState)
 
+  // planId, prefId, password それぞれを GlobalState と LocalStorage へ設定
   useEffect(() => {
     if (typeof planId !== 'string') return
     // planIdをセット
     setPlanId(planId)
 
-    // 「現在のplanIdを先頭とする配列」をLocalStorageに保存
-    const planIdList = localStorage.getItem('planIdList')
-    if (!planIdList) {
-      // planIdListがまだ無い場合
-      localStorage.setItem('planIdList', JSON.stringify([planId]))
-      return
+    // TODO: エラー確認しながら二重条件式のリファクタリング
+    const planListStr = localStorage.getItem('planList')
+    if (!planListStr) {
+      // planListがまだ無いとき
+      sortAndSavePlanList(planId, '')
+    } else {
+      const planList: { id: string; password: string }[] =
+        JSON.parse(planListStr)
+      const index = planList.findIndex((plan) => plan.id === planId)
+      if (index === -1) {
+        // 現在のplanがplanListに無いとき
+        sortAndSavePlanList(planId, '')
+      } else {
+        // 現在のplanがplanListにあるとき
+        const currentPlanPassword = planList[index].password
+        sortAndSavePlanList(planId, currentPlanPassword)
+        setPassword(currentPlanPassword)
+      }
     }
-    // 「現在のplanIdを先頭とする配列」を作成し、上書き
-    const prevList: string[] = JSON.parse(planIdList)
-    const trimmedList = prevList.filter((item) => item !== planId)
-    const newList = [planId, ...trimmedList]
-    localStorage.setItem('planIdList', JSON.stringify(newList))
 
     const localPrefId = localStorage.getItem('prefId')
     if (localPrefId) {
       // prefIdをセット
       setPrefId(localPrefId)
     } else {
-      // 初期値13(東京)をローカルストレージに保存
+      // 初期値13(東京)をprefIdにセット
       localStorage.setItem('prefId', '13')
       setPrefId('13')
     }
-  }, [planId, setPlanId, setPrefId])
+  }, [planId, setPlanId, setPrefId, setPassword])
 
   return null
 }
